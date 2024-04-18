381b5d3bc87453b76f32d116327b2711fc77ef09
Daiki04 <daikiwanwan04@gmail.com>
2024-04-18 10:40:34 +0900

    add ipytest

---
+++ chapters/ch2/src/test_prime_game.py
@@ -0,0 +1,41 @@
+import pytest
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+# Test cases
+@pytest.mark.parametrize("num, expected", [
+    (1, False),
+    (2, True),
+    (3, True),
+    (4, False),
+    (5, True),
+    (6, False),
+    (7, True),
+    (8, False),
+    (9, False),
+    (10, False),
+    (11, True),
+    (12, False),
+    (13, True),
+    (14, False),
+    (15, False),
+    (16, False),
+    (17, True),
+    (18, False),
+    (19, True),
+    (20, False),
+])
+
+def test_is_prime(num, expected):
+    assert is_prime(num) == expected
+
+if __name__ == "__main__":
+    ans = ["P" if is_prime(i) else i for i in range(1, 200+1)]
+
+    print(*ans, sep="\n")
